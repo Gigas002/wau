225c512339c1eac230f3dcde782bb84da55eb082
gigas002 <gigas002@pm.me>
2026-04-22 21:44:00 +0900

    feature: implement repo from template

--- tests/src/curse_tests.rs
+++
@@ -1,8 +0,0 @@
-use libwau::providers::curse::Curse;
-use uuid::Uuid;
-
-#[test]
-fn test_new() {
-    let token = Uuid::new_v4().to_string();
-    let curse_client = Curse::new(&token);
-}
--- tests/src/lib.rs
+++
@@ -1,2 +0,0 @@
-#[cfg(test)]
-pub mod curse_tests;
